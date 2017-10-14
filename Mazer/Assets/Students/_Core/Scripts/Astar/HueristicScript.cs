7d38a687aebb785a227110927ca73588c7dd962f
Laurenz <laurenzriklin@gmx.at>
2017-10-14 18:46:14 -0400

    lr princess

--- Mazer/Assets/Students/_Core/Scripts/Astar/HueristicScript.cs
+++ Mazer/Assets/Students/_Core/Scripts/Astar/HueristicScript.cs
@@ -4,6 +4,6 @@ using System.Collections;
 public class HueristicScript : MonoBehaviour {
 		
 	public virtual float Hueristic(int x, int y, Vector3 start, Vector3 goal, GridScript gridScript){
-		return Random.Range(0, 500);
+		return 0;
 	}
 }
