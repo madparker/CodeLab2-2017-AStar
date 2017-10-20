94121c32810cc235154f91fb37ad5b4444bf6cef
Joelforr_MSI-Stealth <joelforr@gmail.com>
2017-10-20 14:11:52 -0400

    Made a script that extends HueristicScript just to be safe

---
+++ Mazer/Assets/Students/jf3023/Scripts/jf3023Hueristic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jf3023Hueristic : HueristicScript {
+
+    public override float Hueristic(int x, int y, Vector3 start, Vector3 goal, GridScript gridScript)
+    {
+        float dx = Mathf.Abs(x - goal.x);
+        float dy = Mathf.Abs(y - goal.y);
+
+        //Tie breaker, cross product implementation 
+        float dx1 = x - goal.x;
+        float dy1 = y - goal.y;
+        float dx2 = start.x - goal.x;
+        float dy2 = start.y - goal.y;
+        float cross = Mathf.Abs(dx1 * dy2 - dx2 * dy1);
+
+        float hueristic = 1.63f * (dx + dy);
+        hueristic += cross * 0.001f;
+        return hueristic;
+    }
+
+    public override float Hueristic(int x, int y, jAStar.Node start, jAStar.Node goal, GridScript gridScript)
+    {
+        float dx = Mathf.Abs(x - goal.x);
+        float dy = Mathf.Abs(y - goal.y);
+
+        //Tie breaker, cross product implementation 
+        float dx1 = x - goal.x;
+        float dy1 = y - goal.y;
+        float dx2 = start.x - goal.x;
+        float dy2 = start.y - goal.y;
+        float cross = Mathf.Abs(dx1 * dy2 - dx2 * dy1);
+
+        float hueristic = 1.63f * (dx + dy);
+        hueristic += cross * 0.001f;
+        return hueristic;
+    }
+}
