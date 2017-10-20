4f52913c8dfe9b53f1df5bcebe2be3b6c08e8cfe
8ude <crbertelsen@gmail.com>
2017-10-19 21:37:46 -0400

    i don't know why i even try

---
+++ Mazer/Assets/Students/CBertelsen/Scripts/CoreyAstarScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreyAstarScript : AStarScript {
+
+    //I literally can't do a fucking thing with this script without getting
+    //bullshit argumentOutOfRangeExceptions, so fuck that
+}
