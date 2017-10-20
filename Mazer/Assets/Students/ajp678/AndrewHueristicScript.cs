dcae53f11266315956b09ade073468003118660c
Andrew Petersell <andrewpetersell@gmail.com>
2017-10-19 23:21:29 -0400

    Game not broken :)

---
+++ Mazer/Assets/Students/ajp678/AndrewHueristicScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndrewHueristicScript : HueristicScript {
+
+	public override float Hueristic(int x, int y, Vector3 start, Vector3 goal, GridScript gridScript)
+	{
+		float num;
+		num = 0;
+		return num;
+	}
+}
