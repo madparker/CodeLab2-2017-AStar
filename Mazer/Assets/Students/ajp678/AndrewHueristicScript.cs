0960276868892901e3824ac1eabde714aba8c1c7
Andrew Petersell <andrewpetersell@gmail.com>
2017-10-19 23:29:38 -0400

    :/

--- Mazer/Assets/Students/ajp678/AndrewHueristicScript.cs
+++ Mazer/Assets/Students/ajp678/AndrewHueristicScript.cs
@@ -7,7 +7,7 @@ public class AndrewHueristicScript : HueristicScript {
 	public override float Hueristic(int x, int y, Vector3 start, Vector3 goal, GridScript gridScript)
 	{
 		float num;
-		num = 0;
+		num = (Mathf.Abs(x - goal.x)) + (Mathf.Abs(y - goal.y));
 		return num;
 	}
 }
