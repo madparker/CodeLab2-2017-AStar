86123a04f18546483d0415f4373389a3b9699145
Hosni Auji <hosni.auji@gmail.com>
2017-10-19 23:40:21 -0400

    Minor changes

---
+++ Mazer/Assets/Students/_Core/Scripts/Astar/HA_HueristicScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HA_HueristicScript : MonoBehaviour {
+
+	public virtual float Hueristic(int x, int y, Vector3 start, Vector3 goal, GridScript gridScript){
+		//		return Random.Range(0, 500);
+		float xDistance = Mathf.Abs (x - goal.x);
+		float yDistance = Mathf.Abs (y - goal.y);
+
+		float cross = Mathf.Abs (((x - goal.x) * (start.y - goal.y)) - ((start.x - goal.x) * (y - goal.y)));
+
+		float heuristic = gridScript.costs [0] * xDistance + yDistance;
+
+		heuristic += cross * 0.005f;
+
+
+		return heuristic;
+	}
+}
+
